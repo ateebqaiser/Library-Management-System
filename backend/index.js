58395ca963023ef256b804239393e5687581d27a
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-10 09:16:20 +0500

    Add seperate route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-import {viewBook,updateBook,deleteBook,createBook} from './mongoose.js'
+import bookRoutes from './routes/book.js'
+
 
 
 const app = express()
@@ -12,25 +13,8 @@ app.get('/',(req,res)=>{
     res.send("Server Running")
 })
 
-app.get('/book',async(req,res)=>{
-    res.send(await viewBook())
-})
+app.use('/book',bookRoutes)
 
-app.post('/book',async(req,res)=>{
-    const json = req.body
-    res.send(await createBook(json))
-})
-
-app.put('/book/:id',async(req,res)=>{
-    const json = req.body
-    const id = req.params.id
-    res.send(await updateBook(id,json))
-})
-
-app.delete('/book/:id',async(req,res)=>{
-    const id = req.params.id
-    res.send(await deleteBook(id))
-})
 
 const port = 4000
 app.listen(port)
