985d461acc323b40395c11004e7b13c3496ae6fa
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-10 07:48:14 +0500

    Add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import NavBar from '../Bars/navBar/NavBar.jsx'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import HomePage from '../Pages/Home/HomePage.jsx'
+import ViewPage from '../Pages/View/ViewPage.jsx'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome</h1>
+      <BrowserRouter>
+      <NavBar></NavBar>
+        <Routes>
+          <Route path='/' element={<HomePage></HomePage>} ></Route>
+          <Route path='/view' element={<ViewPage></ViewPage>} ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
