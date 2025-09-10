b3eb08dc6f839afe0ade18db71665f98eaabb961
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-10 07:44:19 +0500

    Add NavBar

---
+++ frontend/Bars/navBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NavBar_style.css'
+import { Barcontent } from '../../src/data'
+
+const NavBar = () => {
+  return (
+    <>
+        <nav className='navBar-style'>
+        <li>
+            {Barcontent.map((content)=>
+                <li><a href={content.Link}>{content.title}</a></li>
+            )}
+        </li>
+        </nav>
+    </>
+  )
+}
+
+export default NavBar
