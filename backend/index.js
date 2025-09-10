bea91917264e5faa60feff0dc6af25ad545e979d
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-10 08:10:04 +0500

    Add api

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-import {viewBook} from './mongoose.js'
+import {viewBook,updateBook,deleteBook,createBook} from './mongoose.js'
+
 
 const app = express()
 app.use(cors())
@@ -14,7 +15,21 @@ app.get('/book',async(req,res)=>{
     res.send(await viewBook())
 })
 
+app.post('/book',async(req,res)=>{
+    const json = req.body
+    res.send(await createBook(json))
+})
 
+app.put('/book/:id',async(req,res)=>{
+    const json = req.body
+    const id = req.params.id
+    res.send(await updateBook(id,json))
+})
+
+app.delete('/book/:id',async(req,res)=>{
+    const id = req.params.id
+    res.send(await deleteBook(id))
+})
 
 const port = 4000
 app.listen(port)
