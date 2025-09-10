c066ca52b5811df39f65af6825f1a45f4c9d7143
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-10 08:09:50 +0500

    Add update,delete,create funcs

--- backend/mongoose.js
+++ backend/mongoose.js
@@ -9,3 +9,34 @@ export async function viewBook(){
     const bookfind=await book_collection.find()
     return bookfind
 }
+
+export async function createBook(json){
+    await book_collection.create(
+        {
+            author:json.author,
+            title:json.title,
+            year:json.year
+        }
+    )
+}
+
+export async function updateBook(id,json){
+    await book_collection.updateOne(
+        {
+            _id:id
+        },
+        {
+            author:json.author,
+            title:json.title,
+            year:json.year
+        }
+    )
+}
+
+export async function deleteBook(id){
+    await book_collection.deleteOne(
+        {
+            _id:id
+        }
+    )
+}
