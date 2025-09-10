6e0fd590fcc0424fbfc57bca2e05712b5287652c
ateebqaiser <ateebqaiser243@gmail.com>
2025-09-10 09:16:07 +0500

    Add book route

---
+++ backend/routes/book.js
@@ -0,0 +1,27 @@
+import {viewBook,updateBook,deleteBook,createBook} from '../mongoose.js'
+import express from 'express'
+
+const router=express.Router()
+
+
+router.get('/',async(req,res)=>{
+    res.send(await viewBook())
+})
+
+router.post('/',async(req,res)=>{
+    const json = req.body
+    res.send(await createBook(json))
+})
+
+router.put('/:id',async(req,res)=>{
+    const json = req.body
+    const id = req.params.id
+    res.send(await updateBook(id,json))
+})
+
+router.delete('/:id',async(req,res)=>{
+    const id = req.params.id
+    res.send(await deleteBook(id))
+})
+
+export default router
